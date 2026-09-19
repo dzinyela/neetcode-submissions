0b093a38f76b27ee3f3bfc0103d55ee321012c14
Richard Dzinyela <dzinyela_1@tamu.edu>
2026-09-19 03:23:10 -0500

    Add: python-string-slicing-part-1 - submission-0

---
+++ Python For Beginners/python-string-slicing-part-1/submission-0.py
@@ -0,0 +1,19 @@
+def get_substring(input_string: str, start: int, end: int) -> str:
+
+    a = len(input_string)
+
+    if end<=a:
+        return input_string[start:end]
+    else:
+        return ""
+   
+
+
+
+# do not modify below this line
+print(get_substring("NeetCode", 1, 7))
+print(get_substring("NeetCode", 1, 8))
+print(get_substring("NeetCode", 1, 9))
+print(get_substring("NeetCode", 0, 2))
+print(get_substring("NeetCode", 0, 7))
+print(get_substring("NeetCode", 4, 8))
